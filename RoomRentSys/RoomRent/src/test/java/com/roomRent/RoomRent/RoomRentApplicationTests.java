0c73182ccd53a0336d506befc0025429c2e8dd44
MiteshPrajapat <miteshprajapat33@gmail.com>
2026-02-10 18:06:48 +0530

    Initial commit - Room Rent project

---
+++ RoomRentSys/RoomRent/src/test/java/com/roomRent/RoomRent/RoomRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomRent.RoomRent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
